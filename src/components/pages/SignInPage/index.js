6887b35eb067cfe097a3d6335c533b9221fe6cc7
morghull <morghull@gmail.com>
2021-12-04 18:52:52 +0200

    complete SignInPage

--- src/components/pages/SignInPage/index.js
+++ src/components/pages/SignInPage/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import SignInForm from '../../forms/SignInForm';
+import styles from './SignInPage.module.scss'
 
 class SignInPage extends Component {
   render() {
     return (
-      <div>
-        <h2>login to your account</h2>
+      <div className={styles.contaner}>
+        <h2 className={styles.title}>login to your account</h2>
         <SignInForm />
       </div>
     );
